62c2e29baf756407a32b9209c6633c8b20000380
MarcosVCunha <marcos-cunha@uergs.edu.br>
2020-05-21 20:45:03 -0300

    adicionado pdf do regulamento

--- siepex_app/lib/src/eventos/juergs/RegulamentoPage.dart
+++ siepex_app/lib/src/eventos/juergs/RegulamentoPage.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
 
-class RegulamentoPage extends StatelessWidget {
+class RegulamentoPage extends StatefulWidget {
+  @override
+  _RegulamentoPageState createState() => _RegulamentoPageState();
+}
+
+class _RegulamentoPageState extends State<RegulamentoPage> {
+  String pdfasset = "assets/img/regulamento.pdf";
+  PDFDocument _doc;
+  bool _loading;
+
+  void initState(){
+    super.initState();  
+    _initPdf();
+  }
+
+  _initPdf() async {
+    setState((){
+      _loading = true;
+    });
+    final doc = await PDFDocument.fromAsset(pdfasset);
+    setState(() {
+      _doc = doc;
+      _loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,55 +39,12 @@ class RegulamentoPage extends StatelessWidget {
                 colors: [Color(0xffA1D2E7), Color(0xffB5FDF8)],
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter)),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 14, vertical: 20),
-                child: Text(
-                  'Faça download do regulamento clicando no botão abaixo',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 28,
-                      fontWeight: FontWeight.w600),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      //color: Color(0xff3D20E8),
-                      color: Colors.deepPurple
-                      //gradient: LinearGradient(colors: [Color(0xff5200E8), Color(0xff3111FF)])
-                      ),
-                  height: 60,
-                  width: 180,
-                  child: ListTile(
-                    title: Text(
-                      'Download',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                    ),
-                    trailing: Icon(Icons.file_download, color: Colors.white),
-                    onTap: () {
-                      clicarDownload();
-                    },
-                  )),
-            ],
-          ),
+        child: _loading ? Center(child: CircularProgressIndicator(),) : 
+        PDFViewer(document: _doc,
+          showPicker: false,
         ),
       ),
     );
   }
 
-  clicarDownload() async {
-    const url = 'https://docs.google.com/a/uergs.edu.br/uc?export=download&id=1ALSNBiKKePw0Qz1scWqWAGBIkIV2qc1a';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
 }
